c00630fc6f8ccd8a5561115819a292b36b73f76b
Blithe Rocher <blitherocher@gmail.com>
2012-12-01 12:04:08 -0500

    Finish geolocation

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -2,4 +2,5 @@ class Attendee < ActiveRecord::Base
   attr_accessible :address, :bio, :name, :picture, :twitter_handle
   geocoded_by :address
   after_validation :geocode
+  acts_as_gmappable :process_geocoding => false
 end
